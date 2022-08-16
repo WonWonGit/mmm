ce367e65a0aa60d1057624477c2f81f569882991
WonWonGit <bkn020612@gmail.com>
2022-08-16 11:00:47 +0900

    playList delete style

--- js/playListControl.js
+++ js/playListControl.js
@@ -9,6 +9,9 @@ export class PlayListControl{
 
         this.indexedDB = new IndexedDB();
 
+        this.playListUl = document.querySelector('.ul-playList');
+        this.init();
+
         this.playList = document.querySelector('.nav-playList');
         this.playListBtn = document.querySelector('.listBtn');
         this.nav = document.querySelector('.nav');
@@ -28,46 +31,76 @@ export class PlayListControl{
 
         this.playListBtn.addEventListener('click',this.showPlayList.bind(this));
 
-        this.playListUl = document.querySelector('.ul-playList');
-        
-
         this.fileInput = document.querySelector('.file-input');
         this.fileName = document.querySelector('.file-name');
         this.fileInput.addEventListener('change',(event)=>{this.filesUpload(event)});
 
-        this.init();
+        this.playListEdit = document.querySelector('.edit');
+        this.playListEdit.addEventListener('click', this.editPlayList.bind(this));
+
+        this.playListGrip = document.getElementsByClassName('li-grip');
+        this.playListDelete = document.getElementsByClassName('li-delete');
+
+        this.position = {x:null, y:null};
+        this.diff = {x:null, y:null};
+        this.mouseDown = false;
+        this.seletedItem = null;
+        this.resetTransition = false;
+        this.trasitonTime = 400;
     }
 
-    async init(){
-        this.allPlayList = await this.indexedDB.getAllIndexedPlayList();
-        
-        while (this.playListUl.hasChildNodes()) {
-            this.playListUl.removeChild(this.playListUl.firstChild);
-        }
-
-        this.tracks.innerHTML = `${this.allPlayList.length} tracks`;
-
-        this.allPlayList.forEach((playList,index) => {
-            var insertHtml = `<li class="li-playList" id=${playList.id}>
-                                <div class="disc">
-                                    <img src="/icon/compact-disc-solid.svg">
-                                </div>
-                                <div class="li-index">${(index+1).toString().length=== 1 ? `0${index+1}` : index+1 }</div>
-                                <div class="li-name">${playList.name}</div>
-                                <button id='${playList.id}'>delete</button>
-                              </li>`
-            this.playListUl.insertAdjacentHTML('beforeend',insertHtml);
-            // document.getElementById(`${playList.id}`).addEventListener('click',(event)=>{this.playListDelete(event.target.id)});
-        })
+    getAllPlayList(){
+        return this.allPlayList;
     }
 
-    setUploadMusicFiles (uploadMusicFiles){
+    setAllPlayList(allPlayList){
+        this.allPlayList = allPlayList;
+    }
+
+     setUploadMusicFiles (uploadMusicFiles){
         this.uploadMusicFiles  = uploadMusicFiles ;
     }
 
     getUploadMusicFiles (){
         return this.uploadMusicFiles;
     }
+
+
+    async init(){
+        var allPalyList = await this.indexedDB.getAllIndexedPlayList();
+        this.setAllPlayList(allPalyList);
+
+        while (this.playListUl.hasChildNodes()) {
+            this.playListUl.removeChild(this.playListUl.firstChild);
+        }
+
+        this.tracks.innerHTML = `${this.allPlayList.length} tracks`;
+
+        this.getAllPlayList().forEach((playList,index) => {
+            var insertHtml = `<li class="li-playList" id=${playList.id}>
+                                <div class="disc">
+                                    <img src="/icon/compact-disc-solid.svg">
+                                </div>
+                                <div class="li-index">${(index+1).toString().length=== 1 ? `0${index+1}` : index+1 }</div>
+                                <div class="li-name">${playList.name}</div>
+                                <div class="li-grip fade-in">
+                                    <img src="/icon/grip-lines-solid.svg">
+                                </div>
+                                <div class="li-delete fade-out" id='${playList.id}'>
+                                    <img src="/icon/trash-can-solid.svg">
+                                </div>
+                              </li>`
+            this.playListUl.insertAdjacentHTML('beforeend',insertHtml);
+            // document.getElementById(`${playList.id}`).addEventListener('click',(event)=>{this.playListDelete(event.target.id)});
+        });
+
+        this.playListEdit = document.querySelector('.edit');
+        this.playListGrip = document.getElementsByClassName('li-grip');
+        this.playListDelete = document.getElementsByClassName('li-delete');
+
+        
+    }
+
 
     showPlayList(){
         this.playList.classList.toggle('playList-open');
@@ -140,9 +173,9 @@ export class PlayListControl{
     }
 
     playingMusic(nowPlayingMusicId){
-        console.log(nowPlayingMusicId);
+        
         var playListLi =  document.getElementsByClassName('li-playList');
-        console.log(playListLi);
+        
         [...playListLi].forEach((li) => {
             if(li.id == nowPlayingMusicId){
                 li.style.backgroundColor = '#f3f3f3';
@@ -154,6 +187,38 @@ export class PlayListControl{
             }
         })
         
+    }
+
+    editPlayList(){
+        var editInnerHtml = this.playListEdit.innerHTML;
+        
+        if(editInnerHtml === 'edit'){
+            this.playListEdit.innerHTML = 'done';
+
+            [...this.playListGrip].forEach((grip) => {
+                console.log(grip.classList);
+                grip.classList.remove('fade-in');
+                grip.classList.add('fade-out');
+            });
+
+            [...this.playListDelete].forEach((trash) => {
+                trash.classList.remove('fade-out');
+                trash.classList.add('fade-in');
+            });
+        }else{
+            this.playListEdit.innerHTML = 'edit';
+
+            [...this.playListGrip].forEach((grip) => {
+                console.log(grip.classList);
+                grip.classList.remove('fade-out');
+                grip.classList.add('fade-in');
+            });
+
+            [...this.playListDelete].forEach((trash) => {
+                trash.classList.remove('fade-in');
+                trash.classList.add('fade-out');
+            });
+        }
     }
 }
 
